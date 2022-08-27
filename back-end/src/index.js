5bab583b34af515c2ab9d1b19c89c66362abebe7
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-08-27 14:22:28 -0300

    instalando o cors

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./routes');
 const error = require('./Middleware/middlewareError');
 
@@ -6,6 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/', router);
 app.use(error);
