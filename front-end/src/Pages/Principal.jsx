d151c4c17418ce2aa73cb951881248c865d5c0ff
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-08-27 13:06:20 -0300

    Feat: Criando o componente de card

--- front-end/src/Pages/Principal.jsx
+++ front-end/src/Pages/Principal.jsx
@@ -1,22 +1,38 @@
 import React from 'react';
+import CardBlog from '../Components/CardBlog';
 import Header from '../Components/Header';
 
 function Principal() {
   return (
-    <main>
-      <Header>
-        <div>
-          <input type="text" />
-          <button type="button">Pesquisar</button>
+    <div>
+
+      <header>
+        <Header>
+          <div>
+            <input type="text" />
+            <button type="button">Pesquisar</button>
+          </div>
+          <div>
+            <select>
+              <option value="1">1</option>
+              <option value="2">2</option>
+            </select>
+          </div>
+        </Header>
+      </header>
+
+      <main className="border border-success container text-center">
+        <div className="row row-cols-2 row-cols-lg-5 g-2 g-lg-3">
+          <CardBlog />
+          <CardBlog />
+          <CardBlog />
+          <CardBlog />
+          <CardBlog />
+          <CardBlog />
         </div>
-        <div>
-          <select>
-            <option value="1">1</option>
-            <option value="2">2</option>
-          </select>
-        </div>
-      </Header>
-    </main>
+      </main>
+
+    </div>
   );
 }
 
