4e1d14b59f4e2d2c04dcca99ec0d22c1a1a44ac6
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-08-29 14:15:29 -0300

    Feat: Realizando a requisição para buscar informaçoes no banco de dados - get

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,26 @@
+import { useEffect, useState } from 'react';
 import Rotas from './Router';
 import MyContext from './MyContext/MyContext';
+import { RequestApi } from './Services/RequestApi';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const api = async (query = 'computador') => {
+    setLoading(true);
+    const teste = await RequestApi('/blog')
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    api()
+  },[]);
 
   const contextValue = {
+    data,
+    loading,
   };
 
 
