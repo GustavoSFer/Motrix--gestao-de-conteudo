35eaafaf0ec44a5421b6fafb0a4f13ec371b29dc
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-08-31 14:39:12 -0300

    Test: Verificando a função Update do controller

--- back-end/src/tests/controller/controller.test.jsx
+++ back-end/src/tests/controller/controller.test.jsx
@@ -4,20 +4,56 @@ const serviceBlog = require('../../service/BlogService');
 const controllerBlog = require('../../controller/BlogController');
 const mock = require('../Mock');
 
-describe('# Controller', () => {
-  const response = {};
-  const request = {};
-  before(function () {
-    request.body = { };
-    response.status = Sinon.stub().returns(response);
-    response.json = Sinon.stub().returns();
-  });
+const response = {};
+const request = {};
 
-  describe('# Create', () => {
+describe('# Controller', () => {
+   describe('# Create', () => {
+    before(function () {
+      request.body = { titulo: "realização de teste", corpo: "fazendo uma atualização de teste" };
+      response.status = Sinon.stub().returns(response);
+      response.json = Sinon.stub().returns();
+    });
     it('cadastrando um novo post - Deve retornar o status 201', async () => {
       Sinon.stub(serviceBlog, 'create').resolves(mock.controllerCreate);
       await controllerBlog.create(request, response);
       expect(response.status.calledWith(201)).to.be.equal(true);
+      expect(response.json.calledWith(mock.controllerCreate)).to.be.equal(true);
+      Sinon.restore();
+    });
+  });
+
+  describe('# Update', () => {
+    before(function () {
+      request.body = { corpo: "fazendo uma atualização de teste", blogId: 1 };
+      response.status = Sinon.stub().returns(response);
+      response.json = Sinon.stub().returns();
+    });
+    it('Atualizando um novo post - Deve retornar o status 200', async () => {
+      Sinon.stub(serviceBlog, 'update').resolves({
+        UpdateBlog: {
+          dataValues: {
+            id: 32,
+            corpo: 'fazendo uma atualização de teste',
+            dataAtualizacao: '2022-08-31T17:31:37.393Z',
+            ativo: true,
+            blogId: 1
+          },
+        }
+      });
+      await controllerBlog.update(request, response);
+      expect(response.status.calledWith(200)).to.be.equal(true);
+      expect(response.json.calledWith({
+        UpdateBlog: {
+          dataValues: {
+            id: 32,
+            corpo: 'fazendo uma atualização de teste',
+            dataAtualizacao: '2022-08-31T17:31:37.393Z',
+            ativo: true,
+            blogId: 1
+          },
+        }
+      })).to.be.equal(true);
       Sinon.restore();
     });
   });
