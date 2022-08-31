db0b40b4ed50f6f8b20d54984532eb8b8a0b3da9
Gustavo Fernandes <gustavo.sfernander@gmail.com>
2022-08-31 17:43:12 -0300

    Test: Verificando se os inputs estão na tela

--- front-end/src/tests/PagePrincipal.test.jsx
+++ front-end/src/tests/PagePrincipal.test.jsx
@@ -11,8 +11,25 @@ describe('Verificando se é rederizados os elementos na tela principal', () => {
       const logo = screen.getAllByRole('img');
       expect(logo).toHaveLength(2);
     });
-    it('Verificando se o input de busca esta na tela', () => {
-
+    it('Verificando se existe 3 input na tela', () => {
+      render(<MemoryRouter><App /></MemoryRouter>);
+      const input = screen.getAllByRole('textbox');
+      expect(input).toHaveLength(3);
+    });
+    it('Verificando se o input de "Pesquisa" está na tela', () => {
+      render(<MemoryRouter><App /></MemoryRouter>);
+      const txtPesquisa = screen.getByTestId('Pesquisar por título');
+      expect(txtPesquisa).toBeInTheDocument();
+    });
+    it('Verificando se o input de "Texto" está na tela', () => {
+      render(<MemoryRouter><App /></MemoryRouter>);
+      const txtPesquisa = screen.getByTestId('texto');
+      expect(txtPesquisa).toBeInTheDocument();
+    });
+    it('Verificando se o input de "Título" está na tela', () => {
+      render(<MemoryRouter><App /></MemoryRouter>);
+      const txtPesquisa = screen.getByTestId('Título');
+      expect(txtPesquisa).toBeInTheDocument();
     });
     it('Verificando se o botão de pesquisar esta na tela', () => {
 
